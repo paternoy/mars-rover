7c64d3b198ac83a30a453ca5580b846c642f7b87
Jaume Paternoy <paternoy@gmail.com>
2018-03-08 13:35:32 +0100

    Added REST tests and test cases for Rover instructions execution

---
+++ src/test/java/com/blueliv/rest/RoverRestControllerTest.java
@@ -0,0 +1,125 @@
+package com.blueliv.rest;
+
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.blueliv.RoverApplication;
+import com.blueliv.controller.MarsRoverController;
+import com.blueliv.controller.PlateauResponse;
+import com.blueliv.controller.command.CommandFactoryProvider;
+import com.blueliv.controller.command.CompositeCommand;
+import com.blueliv.controller.command.exception.CommandFormatException;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = RoverApplication.class)
+@WebAppConfiguration
+public class RoverRestControllerTest {
+
+	private static final String EMPTY_BODY = "";
+
+	private static final String ERROR_MESSAGE = EMPTY_BODY;
+
+	private static final String ROVER_ENDPOINT = "/rover";
+
+	private static final String PLAIN_UTF_8_MEDIATYPE = MediaType.TEXT_PLAIN_VALUE + ";charset=UTF-8";
+
+	private static final String REQUEST_BODY_OK = "5 5\n1 3 N\nMMRLR";
+	private static final String REQUEST_BODY_WRONG_NUMBER_OF_LINES = "3 3\n1 3 N\nMMRLR\n1 4 N";
+	private static final String REQUEST_BODY_WRONG_COMMAND_FORMAT = "3 3\n1 3N\nMMRLR";
+	private static final String OK_RESPONSE = "1 5 E";
+
+	private MockMvc mockMvc;
+
+	@InjectMocks
+	RoverRestController roverRestController;
+
+	@Mock
+	MarsRoverController marsRoverControllerMock;
+
+	@Mock
+	CommandFactoryProvider commandFactoryProviderMock;
+
+	@Before
+	public void setup() throws Exception {
+		MockitoAnnotations.initMocks(this);
+		mockMvc = MockMvcBuilders.standaloneSetup(roverRestController).build();
+	}
+
+	@Test
+	public void postCommandsOK() throws Exception {
+		PlateauResponse response = new PlateauResponse(OK_RESPONSE);
+		CompositeCommand command = new CompositeCommand();
+
+		when(commandFactoryProviderMock.parseCommand(same(CompositeCommand.class), eq(REQUEST_BODY_OK)))
+				.thenReturn(command);
+		when(marsRoverControllerMock.processCommands(any(CompositeCommand.class))).thenReturn(response);
+
+		mockMvc.perform(post(ROVER_ENDPOINT).contentType(PLAIN_UTF_8_MEDIATYPE).content(REQUEST_BODY_OK))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(content().contentType(PLAIN_UTF_8_MEDIATYPE))
+				.andExpect(content().string(OK_RESPONSE));
+
+		verify(commandFactoryProviderMock, times(1)).parseCommand(same(CompositeCommand.class), eq(REQUEST_BODY_OK));
+		verify(marsRoverControllerMock, times(1)).processCommands(same(command));
+		verifyNoMoreInteractions(marsRoverControllerMock);
+
+	}
+
+	@Test
+	public void postCommandsEmptyBodyKO() throws Exception {
+
+		mockMvc.perform(post(ROVER_ENDPOINT).contentType(MediaType.TEXT_PLAIN).content(EMPTY_BODY))
+				.andExpect(status().isBadRequest());
+		verifyZeroInteractions(commandFactoryProviderMock);
+		verifyZeroInteractions(marsRoverControllerMock);
+
+	}
+
+	@Test
+	public void postCommandsWrongNumberOfLinesKO() throws Exception {
+		when(commandFactoryProviderMock.parseCommand(same(CompositeCommand.class), any(String.class)))
+				.thenThrow(new CommandFormatException(ERROR_MESSAGE));
+
+		mockMvc.perform(
+				post(ROVER_ENDPOINT).contentType(MediaType.TEXT_PLAIN).content(REQUEST_BODY_WRONG_NUMBER_OF_LINES))
+				.andExpect(status().isBadRequest())
+				.andExpect(content().string(ERROR_MESSAGE));
+
+		verify(commandFactoryProviderMock, times(1)).parseCommand(same(CompositeCommand.class),
+				eq(REQUEST_BODY_WRONG_NUMBER_OF_LINES));
+		verifyZeroInteractions(marsRoverControllerMock);
+	}
+
+	@Test
+	public void postCommandsWrongCommandFormatKO() throws Exception {
+		when(commandFactoryProviderMock.parseCommand(same(CompositeCommand.class), any(String.class)))
+				.thenThrow(new CommandFormatException(ERROR_MESSAGE));
+
+		mockMvc.perform(
+				post(ROVER_ENDPOINT).contentType(MediaType.TEXT_PLAIN).content(REQUEST_BODY_WRONG_COMMAND_FORMAT))
+				.andExpect(status().isBadRequest())
+				.andExpect(content().string(ERROR_MESSAGE));
+
+		verify(commandFactoryProviderMock, times(1)).parseCommand(same(CompositeCommand.class),
+				eq(REQUEST_BODY_WRONG_COMMAND_FORMAT));
+		verifyZeroInteractions(marsRoverControllerMock);
+	}
+
+}
--- src/test/java/com/blueliv/service/PlateauServiceTest.java
+++ src/test/java/com/blueliv/service/PlateauServiceTest.java
@@ -1,7 +1,6 @@
-package com.blueliv.service.impl;
+package com.blueliv.service;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,10 +10,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import com.blueliv.TestConfiguration;
 import com.blueliv.model.Coordinates;
+import com.blueliv.model.Instruction;
 import com.blueliv.model.Orientation;
 import com.blueliv.model.Plateau;
 import com.blueliv.model.Rover;
-import com.blueliv.service.PlateauService;
 import com.blueliv.service.exception.PlateauServiceException;
 
 @RunWith(SpringRunner.class)
@@ -50,7 +49,9 @@ public class PlateauServiceTest {
 			Plateau plateau = new Plateau(new Coordinates(5, 5));
 			Coordinates position = new Coordinates(3, 2);
 			Orientation orientation = Orientation.S;
+
 			Rover rover = plateauService.addRoverToPlateau(plateau, position, orientation);
+
 			assertEquals(3, rover.getPosition().getX());
 			assertEquals(2, rover.getPosition().getY());
 			assertEquals(orientation, rover.getOrientation());
@@ -67,6 +68,7 @@ public class PlateauServiceTest {
 			Orientation orientation = Orientation.S;
 
 			plateauService.addRoverToPlateau(plateau, position, orientation);
+
 			fail("A PlateauServiceException was expected");
 		} catch (PlateauServiceException e) {
 		}
@@ -79,9 +81,113 @@ public class PlateauServiceTest {
 			Coordinates position = new Coordinates(3, 3);
 			Orientation orientation = Orientation.S;
 			plateau.addRover(new Rover(position, orientation));
+
 			plateauService.addRoverToPlateau(plateau, position, orientation);
+
 			fail("A PlateauServiceException was expected");
 		} catch (PlateauServiceException e) {
+		}
+	}
+
+	@Test
+	public void testRoverMoveInstruction() {
+		try {
+			Plateau plateau = new Plateau(new Coordinates(4, 4));
+			Coordinates position = new Coordinates(3, 3);
+			Orientation orientation = Orientation.S;
+			Rover rover = new Rover(position, orientation);
+			plateau.addRover(rover);
+			Instruction[] instructions = new Instruction[] { Instruction.M };
+
+			plateauService.executeRoverInstructions(plateau, rover, instructions);
+
+			assertEquals(3, rover.getPosition().getX());
+			assertEquals(2, rover.getPosition().getY());
+		} catch (PlateauServiceException e) {
+			fail("Unexpected PlateauServiceException thrown when processing move instruction");
+		}
+	}
+
+	@Test
+	public void testRoverMoveInstructionOffLimits() {
+		try {
+			Plateau plateau = new Plateau(new Coordinates(4, 4));
+			Coordinates position = new Coordinates(3, 4);
+			Orientation orientation = Orientation.N;
+			Rover rover = new Rover(position, orientation);
+			plateau.addRover(rover);
+			Instruction[] instructions = new Instruction[] { Instruction.M };
+
+			plateauService.executeRoverInstructions(plateau, rover, instructions);
+
+			assertEquals(3, rover.getPosition().getX());
+			assertEquals(4, rover.getPosition().getY());
+		} catch (PlateauServiceException e) {
+			fail("Unexpected PlateauServiceException thrown when processing move instruction");
+		}
+	}
+
+	@Test
+	public void testRoverMoveInstructionToOccupiedPosition() {
+		try {
+			Plateau plateau = new Plateau(new Coordinates(4, 4));
+			Orientation orientation = Orientation.N;
+			// Rover 1
+			Coordinates position1 = new Coordinates(3, 4);
+			Rover rover1 = new Rover(position1, orientation);
+			plateau.addRover(rover1);
+
+			// Rover 2
+			Coordinates position2 = new Coordinates(3, 3);
+			Rover rover2 = new Rover(position2, orientation);
+			plateau.addRover(rover2);
+			Instruction[] instructions = new Instruction[] { Instruction.M };
+
+			plateauService.executeRoverInstructions(plateau, rover2, instructions);
+
+			assertEquals(position2, rover2.getPosition());
+			assertEquals(Orientation.N, rover2.getOrientation());
+		} catch (PlateauServiceException e) {
+			fail("Unexpected PlateauServiceException thrown when processing MOVE instruction");
+		}
+	}
+
+	@Test
+	public void testRoverTurnLeftInstruction() {
+		try {
+			Plateau plateau = new Plateau(new Coordinates(4, 4));
+			Coordinates position = new Coordinates(3, 4);
+			Orientation orientation = Orientation.N;
+			Rover rover = new Rover(position, orientation);
+			plateau.addRover(rover);
+			Instruction[] instructions = new Instruction[] { Instruction.L };
+
+			plateauService.executeRoverInstructions(plateau, rover, instructions);
+
+			assertEquals(position, rover.getPosition());
+			assertEquals(Orientation.W, rover.getOrientation());
+		} catch (PlateauServiceException e) {
+			fail("Unexpected PlateauServiceException thrown when processing LEFT instruction");
+		}
+	}
+
+	@Test
+	public void testRoverTurnRightInstruction() {
+		try {
+			Plateau plateau = new Plateau(new Coordinates(4, 4));
+			Coordinates position = new Coordinates(3, 4);
+			Orientation orientation = Orientation.N;
+			Rover rover = new Rover(position, orientation);
+			plateau.addRover(rover);
+			Instruction[] instructions = new Instruction[] { Instruction.R, Instruction.R, Instruction.R,
+					Instruction.R };
+
+			plateauService.executeRoverInstructions(plateau, rover, instructions);
+
+			assertEquals(position, rover.getPosition());
+			assertEquals(orientation, rover.getOrientation());
+		} catch (PlateauServiceException e) {
+			fail("Unexpected PlateauServiceException thrown when processing LEFT instruction");
 		}
 	}
 
